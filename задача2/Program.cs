6be6260abfd481056d43493bbb193e0291f50bee
Tatiana <camellia.ts17@gmail.com>
2022-04-16 21:33:25 +0300

    commit2

---
+++ задача2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+using System;
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int max = a;
+if (b > max) max = b;
+
+Console.WriteLine($"max = {max}");
