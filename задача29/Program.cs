579f4dad65657bbfe078cbe506b59f9090e3a561
Tatiana <camellia.ts17@gmail.com>
2022-04-26 16:26:54 +0300

    задача29

---
+++ задача29/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая задаёт массив из n элементов и выводит их на экран.
+
+
+Console.Write("Напишите количество элементов в массиве: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine($"[{string.Join(", ", Array(n))}]");
+
+int [] Array (int num)
+{
+    int [] arr = new int [num];
+    for (int i=0; i < num; i++)
+    {
+        arr [i] = new Random().Next(1, 100);
+    }
+    return arr;
+}
