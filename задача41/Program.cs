713c93ef2bf5e56df07656f3298fdffd99acbdc9
Tatiana <camellia.ts17@gmail.com>
2022-05-05 14:01:12 +0300

    задача41

---
+++ задача41/Program.cs
@@ -0,0 +1,20 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+/* 0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+
+Console.WriteLine("Введите количество чисел: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите числа: ");
+double [] ar = new double [m];
+int count = 0;
+for (int i=0; i< m; i++)
+{
+    ar[i] = double.Parse(Console.ReadLine());
+    Console.WriteLine(String.Join(", " , ar));
+}
+foreach (double a in ar)
+{
+    count += a >3 ? 1 : 0;
+}
+Console.WriteLine($"{String.Join(", " , ar)} - > количество чисел больше 3: {count}");
