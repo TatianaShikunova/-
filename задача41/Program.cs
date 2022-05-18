659824f71e78fa450b229bcba71163cef7a276ff
Tatiana <camellia.ts17@gmail.com>
2022-05-18 21:56:20 +0300

    задача47

--- задача41/Program.cs
+++ задача41/Program.cs
@@ -2,7 +2,8 @@
 /* 0, 7, 8, -2, -2 -> 2
 1, -7, 567, 89, 223-> 3 */
 
-Console.WriteLine("Введите количество чисел: ");
+
+ Console.WriteLine("Введите количество чисел: ");
 int m = int.Parse(Console.ReadLine());
 
 Console.WriteLine("Введите числа: ");
@@ -18,3 +19,5 @@ foreach (double a in ar)
     count += a >3 ? 1 : 0;
 }
 Console.WriteLine($"{String.Join(", " , ar)} - > количество чисел больше 3: {count}");
+
+
---
+++ задача47/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+using System;
+
+Console.Write("Введите количество строк: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов: ");
+int n = int.Parse(Console.ReadLine());
+
+double [,] array = GetArray(m,n);
+for (int k = 0; k < m; k++)
+{
+    for (int l = 0; l< n; l++)
+    {
+        Console.Write($"{array[k,l]} ");
+    }
+    Console.WriteLine();
+}
+
+double [,] GetArray(int rows, int columns)
+{
+    double [,] result = new double [rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j< columns; j++)
+        {
+            result [i,j] = new Random().Next(-10,10);
+        }
+    }
+    return result;
+}
